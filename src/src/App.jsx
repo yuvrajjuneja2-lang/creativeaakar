45f6a9e385b8384e4d06887648d8778b059ab971
yuvrajjuneja2-lang <yuvrajjuneja2@gmail.com>
2026-02-15 01:06:50 +0530

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{
+      background: "black",
+      color: "white",
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "Arial"
+    }}>
+      <div style={{ textAlign: "center" }}>
+        <h1>Creative Aakar</h1>
+        <p style={{ color: "#aaa" }}>
+          Cinematic storytelling for modern brands.
+        </p>
+      </div>
+    </div>
+  );
+}
